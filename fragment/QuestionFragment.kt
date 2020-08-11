511c9dbbc9c2e2be3ee8a1667539b021683c4ebe
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-08-11 18:06:31 +0900

    Add files via upload

---
+++ fragment/QuestionFragment.kt
@@ -0,0 +1,40 @@
+package com.example.test123.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.test123.R
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_question.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuestionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuestionFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        btn_next2.setOnClickListener{
+            navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+        }
+    }
+}
---
+++ fragment/ResultFragment.kt
@@ -0,0 +1,43 @@
+package com.example.test123.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.test123.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ResultFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ResultFragment : Fragment() {
+
+    var option = -1
+
+    lateinit var navController: NavController
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        option = arguments?.getInt("index")?:-1
+
+        return inflater.inflate(R.layout.fragment_result, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+    }
+
+
+}
